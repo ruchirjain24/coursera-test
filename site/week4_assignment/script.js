ca63d8c180579e84673d70d044eca3a1e4b3acbd
unknown <ruchirjain24@gmail.com>
2021-06-12 10:24:50 +0530

    week4_1

--- site/week4_assignment/script.js
+++ site/week4_assignment/script.js
@@ -33,10 +33,10 @@ WARNING!!! WARNING!!!
   for (var i = 0; i < names.length; i++) {
     var firstLetter = names[i].charAt(0).toLowerCase();
     if (firstLetter === 'j') {
-      byeSpeaker.speak(names[i]);
-    } else {
-      helloSpeaker.speak(names[i]);
+      byeSpeaker(names[i]);
+    }
+    else {
+      helloSpeaker(names[i]);
     }
   }
-
-})();
+  })();
