e4a5ecc1230d5610094fac8c77aad574063f0117
unknown <ruchirjain24@gmail.com>
2021-07-05 10:41:22 +0530

    testing api

---
+++ site/api_call trial/index.js
@@ -0,0 +1,48 @@
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin" : "*", 
+            "Access-Control-Allow-Credentials" : true 
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    setTimeout(function(){
+        console.log(response);
+    },2000);
+    return response.json(); // parses JSON response into native JavaScript objects
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // postData('https://nayanapluginserver.pythonanywhere.com/naYana/IPA/getIpaDetect', { 'str': "hello" })
+    //             .then(data => {
+    //                 console.log(data); // JSON data parsed by `data.json()` call
+    //             });
+const Http = new XMLHttpRequest();
+const url='https://nayanapluginserver.pythonanywhere.com/naYana/IPA/getIpaDetect';
+Http.open("POST", url);
+let obj={ str: "hello" };
+obj=JSON.stringify(obj);
+Http.send(obj);
+
+Http.onreadystatechange = (e) => {
+  console.log(Http.responseText)
+}
+    let btn = document.getElementById('btn');
+    btn.addEventListener('click', () => {
+        for (let i = 1; i < 7; i++) {
+            let para = document.getElementById("work" + i);
+            let str = para.innerText;
+            console.log(str);
+        }
+    });
+});
--- site/extend/content_script.js
+++ site/extend/content_script.js
@@ -1,29 +1,42 @@
 function reverseString(str) {
-    var splitString = str.split(""); 
-    var reverseArray = splitString.reverse(); 
+    var splitString = str.split("");
+    var reverseArray = splitString.reverse();
     var joinArray = reverseArray.join("");
-    return joinArray; 
-  }
+    return joinArray;
+}
+function legitOrNot(str) {
+    for (let i = 0; i < str.length; i++) {
+        if(str.charAt(i)==='\n')
+            continue;
+        if(str.charAt(i)==='\t')
+            continue;
+        if(str.charAt(i)===' ')
+            continue;
+        return 1;
+    }
+    return 0;
+}
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.message === "translate") {
 
-    const nynfont = new FontFace('nayana', 'url(https://www.gnowledge.org/assets/fonts/naYanakamikRegular.otf)');
-    nynfont.load();
-    document.fonts.add(nynfont);
+        const nynfont = new FontFace('nayana', 'url(https://www.gnowledge.org/assets/fonts/naYanakamikRegular.otf)');
+        nynfont.load();
+        document.fonts.add(nynfont);
 
-    var style = document.createElement('style');
-    style.innerHTML = `
+        var style = document.createElement('style');
+        style.innerHTML = `
     @font-face {
 			font-family: 'naYanakamikRegular';
 			src: url("https://www.gnowledge.org/assets/fonts/naYanakamikRegular.otf") format("opentype");
 		}`;
-    document.head.appendChild(style);
-    document.body.style.fontFamily='nayana';
+        document.head.appendChild(style);
+        // document.body.style.fontFamily = 'nayana';
+        document.body.style.setProperty('font-family', 'nayana', 'important');
 
-    replaceText(document.body);
-    console.log(document);
-    // document.body.style.fontFamily='nayana !important';
-    console.log(document.body.style);
+        replaceText(document.body);
+        console.log(document);
+        // document.body.style.fontFamily='nayana !important';
+        console.log(document.body.style);
     }
 
     else if (request.message === "error") {
@@ -34,8 +47,10 @@ function replaceText(element) {
     if (element.hasChildNodes()) {
         element.childNodes.forEach(replaceText);
     } else {
-        if(element.textContent.length!==0){
-            // element.textContent = reverseString(element.textContent);
+        if (element.textContent.length !== 0 && legitOrNot(element.textContent)) {
+            element.textContent = "hi "+element.textContent + " bye";
+            let str = element.textContent.split("");
+            console.log(str);
         }
     }
 }
